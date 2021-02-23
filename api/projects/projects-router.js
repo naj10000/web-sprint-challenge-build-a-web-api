2626f6843d766503682b675e0269721fd0473f84
NaJ <naj10000@live.com>
2021-02-22 23:14:51 -0500

    projects

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -25,4 +25,34 @@ router.post('/', validateProjectBody, async(req, res, next) => {
     }
 })
 
+router.put('/:id', validateProjectBody, validateProjectId, async(req, res, next) => {
+    try {
+        const updateProjects = await projectsModel.update(req.params.id, req.body)
+        res.status(201).json(updateProjects)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.delete('/:id', validateProjectId, async(req, res, next) => {
+    try {
+        const deletedProject = await projectsModel.remove(req.params.id)
+        if(deletedProject === 1) {
+            res.status(204).json(deletedProject)
+        }
+        next(err)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.get('/:id/actions', validateProjectId, async(req, res, next) => {
+    try {
+        const projectActions = await projectsModel.getProjectActions(req.params.id)
+        res.status(200).json(projectsModel)
+    } catch (err) {
+        next(err)
+    }
+})
+
 module.exports = router
